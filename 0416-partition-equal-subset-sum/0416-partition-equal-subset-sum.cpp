d4cf7a73e12fccc3ddb994cdee3d621029472550
Kxt-R <krishrauniyar0987@gmail.com>
2026-08-01 00:10:36 +0530

    Time: 4 ms (99.33%), Space: 14.2 MB (79.65%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    int total_sum;
-    bool dp[203][20003];
-    bool visited[203][20003];
-    int run(int idx,int sum,vector<int> &nums){
-        if(idx==nums.size()) return false;
-
-        if(total_sum==2*sum) return true;
-
-        if(visited[idx][sum]) return dp[idx][sum];
-        visited[idx][sum]=true;
-        return run(idx+1,sum+nums[idx],nums) | run(idx+1,sum,nums);
-    }
     bool canPartition(vector<int>& nums) {
-        total_sum=0;
-        for(auto ele: nums) total_sum+=ele;
-        if(total_sum%2==1) return false;
-        memset(dp,false,sizeof dp);
-        memset(visited,false,sizeof visited);
-        return run(0,0,nums);
+        int total=accumulate(nums.begin(),nums.end(),0);
+        if(total%2==1) return false;
+        bitset<10005> bit;
+        bit[0]=1;
+        int sum=total/2;
+        for(auto ele : nums){
+            bit|= (bit<<ele);
+        }
+        if(bit[sum]) return 1;
+        else return 0;
     }
 };
