0e5ddcece06077b97fb2852a9b42eef6502856d6
Kxt-R <krishrauniyar0987@gmail.com>
2026-08-22 14:28:49 +0530

    Time: 1 ms (7.72%), Space: 13.3 MB (60.9%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int n=arr.size();
-        int high=2002,low=1;
+        int high=n-1,low=0;
+        if(arr[0]>k) return k;
         while(high>low){
             int mid=(high+low+1)/2;
-            auto it=lower_bound(arr.begin(),arr.end(),mid)-arr.begin();
-            if(mid-it<=k){
+            cout<<low<<' '<<high<<' '<<mid<<'\n';
+            if(arr[mid]-mid-1<k){
+                cout<<'h'<<'\n';
                 low=mid;
             }
             else{
+                cout<<'j'<<'\n';
                 high=mid-1;
             }
         }
-        return low;
+        return k+low+1;
     }
 };
