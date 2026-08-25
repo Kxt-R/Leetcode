6afe5799d0e66a1a69bda6c21b98e998400c1940
Kxt-R <krishrauniyar0987@gmail.com>
2026-08-26 02:36:44 +0530

    Time: 3 ms (68.19%), Space: 13.1 MB (82.76%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    void helper(int n,int count,string s,vector<string> &ans){
+    void helper(int n,int count,string &s,vector<string> &ans){
         if(count==0 and n==0) {
             ans.push_back(s);
             return;
         }
-        if(count>0) helper(n,count-1,s+')',ans);
-        if(n>0) helper(n-1,count+1,s+'(',ans);
+        if(count>0) {
+            s.push_back(')');
+            helper(n,count-1,s,ans);
+            s.pop_back();
+        }
+        if(n>0) {
+            s.push_back('(');
+            helper(n-1,count+1,s,ans);
+            s.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        helper(n,0,"",ans);
+        string s="";
+        helper(n,0,s,ans);
         return ans;
     }
 };
