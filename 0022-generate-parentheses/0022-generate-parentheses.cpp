641fa9a428c88ba13dc1446e0d8395a98d3bf8b0
Kxt-R <krishrauniyar0987@gmail.com>
2026-08-26 02:29:59 +0530

    Time: 0 ms (100%), Space: 15.6 MB (51.34%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void helper(int n,int count,string s,vector<string> &ans){
+        if(count==0 and n==0) ans.push_back(s);
+        if(count>0) helper(n,count-1,s+')',ans);
+        if(n>0) helper(n-1,count+1,s+'(',ans);
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        helper(n,0,"",ans);
+        return ans;
+    }
+};
