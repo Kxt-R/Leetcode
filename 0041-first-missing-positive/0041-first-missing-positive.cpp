997b0ff67b0d0fe9d016aee78bebe599a493ab42
Kxt-R <krishrauniyar0987@gmail.com>
2026-08-30 18:54:15 +0530

    Time: 4 ms (41.44%), Space: 55.1 MB (31.23%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    int MAX=1000001;
     int firstMissingPositive(vector<int>& nums) {
-        vector<bool> v(MAX,0);
         int n=nums.size();
-        for(int i=0;i<n;++i){
-            if(nums[i]>0 and nums[i]<MAX) v[nums[i]]=1;
+        int i=0;
+        while(i<n){
+            if(nums[i]==i+1 or nums[i]<=0 or nums[i]>=n+1 or nums[i]==nums[nums[i]-1]){
+                ++i;
+                continue;
+            } 
+            swap(nums[i],nums[nums[i]-1]);
         }
-        for(int i=1;i<MAX;++i) if(v[i]!=1) return i;
-        return MAX;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1) return i+1;
+        }
+        return n+1;
     }
 };
