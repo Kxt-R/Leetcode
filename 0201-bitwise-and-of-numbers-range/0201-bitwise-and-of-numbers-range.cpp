81247210a35c8c66400529fbe8a1bafda8f454c6
Kxt-R <krishrauniyar0987@gmail.com>
2026-09-03 00:44:12 +0530

    Time: 1 ms (43.32%), Space: 11.2 MB (80.38%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -5,8 +5,11 @@ public:
         int msb2=31-__builtin_clz(right);
         int ans=0;
         while(msb2>=0){
-            if((right>>msb2)&1==1 and (left>>msb2)&1==1) ans+=1<<msb2;
-            else if((right>>msb2)&1!=(left>>msb2)&1) break;
+            if(((right>>msb2)&1)==1 and ((left>>msb2)&1)==1) ans+=1<<msb2;
+            else if(((right>>msb2)&1)==0 and ((left>>msb2)&1)==0) {
+
+            }
+            else break;
             msb2--;
         }
         return ans;
