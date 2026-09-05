77065fbc4c80c92f5d595c23905064956a4bedb3
Kxt-R <krishrauniyar0987@gmail.com>
2026-09-05 14:12:53 +0530

    Time: 3 ms (95.01%), Space: 202.5 MB (85.02%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n=nums.size();
-        int mx=nums[0],mn;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> h;
-        for(int i=0;i<n;i++) h.push({nums[i],i});
-        for(int i=0;i<n;i++){
-            while(1){
-                pair<int,int> p=h.top();
-                if(p.second<i) h.pop();
-                else break;
-            }
-            mx=max(mx,nums[i]);
-            mn=h.top().first;
-            if(mx-mn<=k) return i;
+        int mx=nums[0];
+        vector<int> mn(n);
+        int m=nums[n-1];
+        for(int i=n-1;i>=0;--i){
+            m=min(m,nums[i]);
+            mn[i]=m;
+        }
+        for(int i=0;i<n;++i){
+            mx=max(mx,m=nums[i]);
+            m=mn[i];
+            if(mx-m<=k) return i;
         }
         return -1;
     }
